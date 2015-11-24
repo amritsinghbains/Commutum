df221b901d844b0be8997ec800f326fe6b177e58
amrit singh bains <amrit.singh.bains@live.com>
2015-11-24 09:50:34 -0500

    new branch for deplyment

---
+++ node.js
@@ -0,0 +1,240 @@
+//localhost:5000?overview_polyline=[{"lat":42.33138,"lng":-83.04575000000001},{"lat":42.33138,"lng":-83.04474},{"lat":42.3314,"lng":-83.04459000000001},{"lat":42.331430000000005,"lng":-83.04447},{"lat":42.331430000000005,"lng":-83.04390000000001},{"lat":42.33144,"lng":-83.04341000000001},{"lat":42.33128000000001,"lng":-83.04328000000001},{"lat":42.330960000000005,"lng":-83.04301000000001},{"lat":42.33052,"lng":-83.04267},{"lat":42.33017,"lng":-83.0424},{"lat":42.329860000000004,"lng":-83.04216000000001},{"lat":42.329480000000004,"lng":-83.04303},{"lat":42.328790000000005,"lng":-83.04465},{"lat":42.32867,"lng":-83.04492},{"lat":42.328340000000004,"lng":-83.04549},{"lat":42.32726,"lng":-83.048},{"lat":42.326890000000006,"lng":-83.04882},{"lat":42.32659,"lng":-83.04959000000001},{"lat":42.32647,"lng":-83.04998},{"lat":42.32645,"lng":-83.05025},{"lat":42.326420000000006,"lng":-83.05155},{"lat":42.326480000000004,"lng":-83.05260000000001},{"lat":42.326570000000004,"lng":-83.05351},{"lat":42.326640000000005,"lng":-83.05385000000001},{"lat":42.32683,"lng":-83.05453},{"lat":42.326910000000005,"lng":-83.05471},{"lat":42.327220000000004,"lng":-83.05550000000001},{"lat":42.32790000000001,"lng":-83.05664},{"lat":42.328590000000005,"lng":-83.05762},{"lat":42.32958,"lng":-83.05892},{"lat":42.330090000000006,"lng":-83.05953000000001},{"lat":42.33088,"lng":-83.0604},{"lat":42.331610000000005,"lng":-83.06115000000001},{"lat":42.331880000000005,"lng":-83.06144},{"lat":42.33214,"lng":-83.06168000000001},{"lat":42.33259,"lng":-83.06205000000001},{"lat":42.332980000000006,"lng":-83.06241},{"lat":42.333780000000004,"lng":-83.06306000000001},{"lat":42.33437000000001,"lng":-83.06351000000001},{"lat":42.33471,"lng":-83.06376},{"lat":42.33529,"lng":-83.06415000000001},{"lat":42.335460000000005,"lng":-83.06425},{"lat":42.33587000000001,"lng":-83.06449},{"lat":42.3361,"lng":-83.06463000000001},{"lat":42.336200000000005,"lng":-83.06466},{"lat":42.336670000000005,"lng":-83.06492},{"lat":42.337700000000005,"lng":-83.06545000000001},{"lat":42.33892,"lng":-83.066},{"lat":42.339360000000006,"lng":-83.06620000000001},{"lat":42.34378,"lng":-83.06821000000001},{"lat":42.34892000000001,"lng":-83.07054000000001},{"lat":42.349630000000005,"lng":-83.07091000000001},{"lat":42.351530000000004,"lng":-83.07202000000001},{"lat":42.35394,"lng":-83.07344},{"lat":42.354510000000005,"lng":-83.07379},{"lat":42.35511,"lng":-83.07413000000001},{"lat":42.355560000000004,"lng":-83.07434},{"lat":42.356170000000006,"lng":-83.07458000000001},{"lat":42.356730000000006,"lng":-83.07478},{"lat":42.35737,"lng":-83.07505},{"lat":42.357780000000005,"lng":-83.07526},{"lat":42.358340000000005,"lng":-83.07558},{"lat":42.35864,"lng":-83.07577},{"lat":42.359300000000005,"lng":-83.07613},{"lat":42.359680000000004,"lng":-83.07638},{"lat":42.36072,"lng":-83.07699000000001},{"lat":42.36128,"lng":-83.07736000000001},{"lat":42.36206000000001,"lng":-83.07796},{"lat":42.364000000000004,"lng":-83.07941000000001},{"lat":42.36511,"lng":-83.08024},{"lat":42.36621,"lng":-83.08109},{"lat":42.366760000000006,"lng":-83.08154},{"lat":42.36703000000001,"lng":-83.08172},{"lat":42.36728,"lng":-83.08189},{"lat":42.370180000000005,"lng":-83.08375000000001},{"lat":42.37111,"lng":-83.08437},{"lat":42.37574,"lng":-83.08748000000001},{"lat":42.378060000000005,"lng":-83.08904000000001},{"lat":42.37923000000001,"lng":-83.08986},{"lat":42.37973,"lng":-83.09021000000001},{"lat":42.38006,"lng":-83.09047000000001},{"lat":42.38051,"lng":-83.09085},{"lat":42.380750000000006,"lng":-83.09112},{"lat":42.381,"lng":-83.09142000000001},{"lat":42.381370000000004,"lng":-83.09197},{"lat":42.38157,"lng":-83.09234000000001},{"lat":42.38177,"lng":-83.09273},{"lat":42.381910000000005,"lng":-83.09306000000001},{"lat":42.382110000000004,"lng":-83.09365000000001},{"lat":42.38242,"lng":-83.09471},{"lat":42.38259,"lng":-83.09530000000001},{"lat":42.38309,"lng":-83.09702},{"lat":42.383280000000006,"lng":-83.09753},{"lat":42.383610000000004,"lng":-83.09825000000001},{"lat":42.383860000000006,"lng":-83.09870000000001},{"lat":42.38421,"lng":-83.09922},{"lat":42.38448,"lng":-83.09957},{"lat":42.38474,"lng":-83.09986},{"lat":42.3851,"lng":-83.10021},{"lat":42.38532,"lng":-83.10041000000001},{"lat":42.38573,"lng":-83.1007},{"lat":42.387710000000006,"lng":-83.10202000000001},{"lat":42.388090000000005,"lng":-83.10227},{"lat":42.389160000000004,"lng":-83.10300000000001},{"lat":42.39014,"lng":-83.10372000000001},{"lat":42.39266000000001,"lng":-83.10558},{"lat":42.393,"lng":-83.10582000000001},{"lat":42.393100000000004,"lng":-83.10583000000001},{"lat":42.39338,"lng":-83.10598},{"lat":42.393750000000004,"lng":-83.10616},{"lat":42.39423,"lng":-83.10638},{"lat":42.394490000000005,"lng":-83.10652},{"lat":42.39486,"lng":-83.10676000000001},{"lat":42.39502,"lng":-83.10687},{"lat":42.39493,"lng":-83.10709000000001},{"lat":42.394830000000006,"lng":-83.10736},{"lat":42.39471,"lng":-83.10770000000001},{"lat":42.394090000000006,"lng":-83.10940000000001},{"lat":42.39385,"lng":-83.11006},{"lat":42.393420000000006,"lng":-83.11121},{"lat":42.39334,"lng":-83.11138000000001},{"lat":42.392410000000005,"lng":-83.11387},{"lat":42.39202,"lng":-83.11491000000001},{"lat":42.39199000000001,"lng":-83.11512},{"lat":42.39108,"lng":-83.11760000000001}]
+var express = require('express');
+var http = require('http');
+var url = require('url');
+var bodyParser = require('body-parser');
+var app = express();
+var pg = require('pg');
+var port = process.env.PORT || 5000
+var fs = require('fs');
+var parse = require('csv-parse');
+var async = require('async');
+var Parallel = require('paralleljs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Authorization, Content-Type');
+  next();
+});
+
+app.listen(port);
+console.log('Listening at ' + port);
+
+var LARCENY = 0.4;
+var AGGRAVATED_ASSAULT = 0.9;
+var ROBBERY = 0.6;
+var BURGLARY = 0.7;
+var STOLEN_VEHICLE = 0.8;
+var HOMICIDE = 1;
+
+var TOTALLARCENY = 13732;
+var TOTALAGGRAVATED_ASSAULT = 8102;
+var TOTALROBBERY = 3109;
+var TOTALBURGLARY = 7461;
+var TOTALSTOLEN_VEHICLE = 6759;
+var TOTALHOMICIDE = 250;
+
+var vLARCENY = [];
+var vAGGRAVATED_ASSAULT = [];
+var vROBBERY = [];
+var vBURGLARY = [];
+var vSTOLEN_VEHICLE = [];
+var vHOMICIDE = [];
+
+var vvLARCENY = 0;
+var vvAGGRAVATED_ASSAULT = 0;
+var vvROBBERY = 0;
+var vvBURGLARY = 0;
+var vvSTOLEN_VEHICLE = 0;
+var vvHOMICIDE = 0;
+
+var vtotalDistance = 0;
+var safetyQ = 0;
+var resultArray = {};
+var saveData = [];
+
+app.get('/', function (req, res) {
+    totalDistance = 0;
+    vLARCENY = [];
+    vAGGRAVATED_ASSAULT = [];
+    vROBBERY = [];
+    vBURGLARY = [];
+    vSTOLEN_VEHICLE = [];
+    vHOMICIDE = [];
+    vtotalDistance = 0;
+    safetyQ = 0;
+    resultArray = {};
+
+    if(req.query.overview_polyline != undefined){
+        var isExists = saveData.containsPolyline(req.query.overview_polyline);
+        if(isExists != false){
+            res.writeHead(200, {
+                'content-type': 'text/json'
+            });
+            res.write(JSON.stringify(isExists))
+            res.end('\n')
+            return 1;
+        }else{
+            console.log('nope')
+        }
+    	overview_polyline = JSON.parse(req.query.overview_polyline);
+    	for(var i=0; i<overview_polyline.length; i++){
+    		getDistance(overview_polyline[i].lat, overview_polyline[i].lng)
+    	}
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+    // console.log("TotalDistance=" + totalDistance)
+    // console.log("NumberOfPoints=" + overview_polyline.length)   
+    
+    // console.log("Actual Number");
+    // console.log("LARCENY: " + vLARCENY.unique().length);
+    // console.log("AGGRAVATED ASSAULT: " + vAGGRAVATED_ASSAULT.unique().length);
+    // console.log("ROBBERY: " + vROBBERY.unique().length);
+    // console.log("BURGLARY: " + vBURGLARY.unique().length);
+    // console.log("STOLEN VEHICLE: " + vSTOLEN_VEHICLE.unique().length);
+    // console.log("HOMICIDE: " + vHOMICIDE.unique().length);
+
+    console.log("Percentage Number");
+    console.log("LARCENY: " + 100*vLARCENY.unique().length/TOTALLARCENY);
+    console.log("AGGRAVATED ASSAULT: " + 100*vAGGRAVATED_ASSAULT.unique().length/TOTALAGGRAVATED_ASSAULT);
+    console.log("ROBBERY: " + 100*vROBBERY.unique().length/TOTALROBBERY);
+    console.log("BURGLARY: " + 100*vBURGLARY.unique().length/TOTALBURGLARY);
+    console.log("STOLEN VEHICLE: " + 100*vSTOLEN_VEHICLE.unique().length/TOTALSTOLEN_VEHICLE);
+    console.log("HOMICIDE: " + 100*vHOMICIDE.unique().length/TOTALHOMICIDE);
+
+    safetyQ += LARCENY*100*vLARCENY.unique().length/TOTALLARCENY;
+    safetyQ += AGGRAVATED_ASSAULT*100*vAGGRAVATED_ASSAULT.unique().length/TOTALAGGRAVATED_ASSAULT;
+    safetyQ += ROBBERY*100*vROBBERY.unique().length/TOTALROBBERY;
+    safetyQ += BURGLARY*100*vBURGLARY.unique().length/TOTALBURGLARY;
+    safetyQ += STOLEN_VEHICLE*100*vSTOLEN_VEHICLE.unique().length/TOTALSTOLEN_VEHICLE;
+    safetyQ += HOMICIDE*100*vHOMICIDE.unique().length/TOTALHOMICIDE;
+
+    console.log("safetyQ: " + safetyQ);
+    resultArray = {
+        polyline: req.query.overview_polyline,
+        safetyQ: 100-safetyQ,
+        LARCENY: 100*vLARCENY.unique().length/TOTALLARCENY,
+        AGGRAVATED_ASSAULT: 100*vAGGRAVATED_ASSAULT.unique().length/TOTALAGGRAVATED_ASSAULT,
+        ROBBERY: 100*vROBBERY.unique().length/TOTALROBBERY,
+        BURGLARY: 100*vBURGLARY.unique().length/TOTALBURGLARY,
+        STOLEN_VEHICLE: 100*vSTOLEN_VEHICLE.unique().length/TOTALSTOLEN_VEHICLE,
+        HOMICIDE: 100*vHOMICIDE.unique().length/TOTALHOMICIDE
+    }
+    res.writeHead(200, {
+        'content-type': 'text/json'
+    });
+    res.write(JSON.stringify(resultArray))
+        //     res.write(JSON.stringify());
+    res.end('\n')
+    saveData.push(resultArray);
+
+});
+
+function getDistance (lat, lng) {	
+	for(var j=0; j<records.length; j++){
+
+		if(getDistanceFromLatLonInKm(lat, lng, parseFloat(records[j].lat), parseFloat(records[j].lng), 'K') < 0.005){
+		    totalDistance++;
+		    switch(records[j].category){
+                case "LARCENY": vLARCENY.push(j); break;
+                case "AGGRAVATED ASSAULT": vAGGRAVATED_ASSAULT.push(j); break;
+                case "ROBBERY": vROBBERY.push(j); break;
+                case "BURGLARY": vBURGLARY.push(j); break;
+                case "STOLEN VEHICLE": vSTOLEN_VEHICLE.push(j); break;
+                case "HOMICIDE": vHOMICIDE.push(j); break;
+            }
+        }
+	}	
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2, unit) {
+	var radlat1 = Math.PI * lat1/180
+	var radlat2 = Math.PI * lat2/180
+	var radlon1 = Math.PI * lon1/180
+	var radlon2 = Math.PI * lon2/180
+
+	var theta = lon1-lon2
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	if (unit=="K") { dist = dist * 1.609344 }
+	if (unit=="N") { dist = dist * 0.8684 }
+	if(isNaN(dist)){
+		return 0;
+	}
+	return dist
+}
+
+Array.prototype.containsPolyline = function(v) {
+    for(var i = 0; i < this.length; i++) {
+        if(this[i].polyline === v) return this[i];
+    }
+    return false;
+};
+
+Array.prototype.contains = function(v) {
+    for(var i = 0; i < this.length; i++) {
+        if(this[i] === v) return true;
+    }
+    return false;
+};
+
+Array.prototype.unique = function() {
+    var arr = [];
+    for(var i = 0; i < this.length; i++) {
+        if(!arr.contains(this[i])) {
+            arr.push(this[i]);
+        }
+    }
+    return arr; 
+}
+
+var category = [];
+
+var records = [];
+
+var inputFile='Major_Crime.csv';
+
+var parser = parse({delimiter: ','}, function (err, data) {
+  async.eachSeries(data, function (line, callback) {
+  	var x = line[11];
+  	// console.log(records.length)
+  	var start_pos1 = x.indexOf('(') + 1;
+	var end_pos1 = x.indexOf(',',start_pos1);
+
+	var start_pos2 = x.indexOf(',') + 2;
+	var end_pos2 = x.indexOf(')',start_pos2);
+
+  	// console.log(x.substring(start_pos1, end_pos1), x.substring(start_pos2, end_pos2))
+  	records.push({
+      	lat: x.substring(start_pos1, end_pos1),
+        lng: x.substring(start_pos2, end_pos2),
+        date: line[4],
+        category: line[1]
+    });
+    if(x.substring(start_pos1, end_pos1).length < 10 && x.substring(start_pos2, end_pos2).length < 10)
+    console.log(x.substring(start_pos1, end_pos1)+',' + x.substring(start_pos2, end_pos2))
+    // switch(line[1]){
+    //     case "LARCENY": vvLARCENY++; break;
+    //     case "AGGRAVATED ASSAULT": vvAGGRAVATED_ASSAULT++; break;
+    //     case "ROBBERY": vvROBBERY++; break;
+    //     case "BURGLARY": vvBURGLARY++; break;
+    //     case "STOLEN VEHICLE": vvSTOLEN_VEHICLE++; break;
+    //     case "HOMICIDE": vvHOMICIDE++; break;
+    //   }
+      // if(line[121] == "bonniepalmer@msn.com"){
+      
+      // console.log(line[121])
+  // }
+      callback();
+    // });
+  });
+}
+);
+fs.createReadStream(inputFile).pipe(parser);
